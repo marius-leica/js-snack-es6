5361ad0162cd2296c4e43de472a7044285717f3a
marius-leica <mariusleica@gmail.com>
2022-06-06 15:44:46 +0200

    Snack 01

--- snack01/app.js
+++ snack01/app.js
@@ -5,9 +5,30 @@ Stampare a schermo la bici con peso minore utilizzando destructuring e template 
 */
 
 const bici = [
-  { nome: "Bici da corsa", peso: "1.5kg" },
-  { nome: "Bici da corsa", peso: "1.5kg" },
-  { nome: "Bici da corsa", peso: "1.5kg" },
-  { nome: "Bici da corsa", peso: "1.5kg" },
-  { nome: "Bici da corsa", peso: "1.5kg" },
+  { nome: "SAVADECK Warwind5.0", peso: 10 },
+  { nome: "SAVADECK Phantom3.0", peso: 7 },
+  { nome: "SHOCKBLAZE BK11SB4041", peso: 9 },
+  { nome: "TREK Emonda SL 5", peso: 8 },
+  { nome: "Helliot Bikes Sport 02", peso: 14 },
 ];
+
+console.table(bici);
+
+const title = document.getElementById("title");
+const wheight = document.getElementById("wheight");
+
+const { nome, peso } = bici[0];
+let biciPiuLeggera = { nome, peso, indice: 0 };
+console.log(biciPiuLeggera);
+
+for (let i = 1; i < bici.length; i++) {
+  const { peso, nome } = bici[i];
+
+  if (peso < biciPiuLeggera.peso) {
+    biciPiuLeggera = { nome, peso, indice: i };
+  }
+}
+
+//stampa
+title.innerHTML = `Bici piu' leggera: ${biciPiuLeggera.nome}`;
+wheight.innerHTML = `Peso: ${biciPiuLeggera.peso} kg.`;
