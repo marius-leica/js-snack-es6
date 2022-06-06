41f3511d3a7eda4a9cbd813ae2318d01fc3d2f12
marius-leica <mariusleica@gmail.com>
2022-06-06 14:25:58 +0200

    first commit

---
+++ snack01/app.js
@@ -0,0 +1,13 @@
+/*
+Creare un array di oggetti:
+Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+Stampare a schermo la bici con peso minore utilizzando destructuring e template literal
+*/
+
+const bici = [
+  { nome: "Bici da corsa", peso: "1.5kg" },
+  { nome: "Bici da corsa", peso: "1.5kg" },
+  { nome: "Bici da corsa", peso: "1.5kg" },
+  { nome: "Bici da corsa", peso: "1.5kg" },
+  { nome: "Bici da corsa", peso: "1.5kg" },
+];
