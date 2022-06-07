31410f3dcaba32f255db1cba97868937a60870f5
marius-leica <mariusleica@gmail.com>
2022-06-07 14:50:22 +0200

    SNack 04 Done

--- snack04/app.js
+++ snack04/app.js
@@ -13,3 +13,33 @@ let students = [
 2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
 3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
 */
+
+let students = [
+  { name: "Marco", id: 213, grades: 78 },
+  { name: "Paola", id: 110, grades: 96 },
+  { name: "Andrea", id: 250, grades: 48 },
+  { name: "Gaia", id: 145, grades: 74 },
+  { name: "Luigi", id: 196, grades: 68 },
+  { name: "Piero", id: 102, grades: 50 },
+  { name: "Francesca", id: 120, grades: 84 },
+];
+//1- Dobbiamo creare delle targhe con il loro nome in maiuscolo. Ci serve quindi un nuovo array di stringhe. Ogni elemento del nuovo array sarà il nome dello studente ma con tutte le lettere maiuscole.
+
+const studenti = students.map((studente) => {
+  return studente.name.toUpperCase();
+});
+console.log(studenti);
+
+//2-  Dobbiamo creare un nuovo array con gli studenti che hanno un totale di voti superiore a 70
+
+const studenti70 = students.filter((studente) => {
+  return studente.grades > 70;
+});
+console.table(studenti70);
+
+//3-  Dobbiamo creare un nuovo array di tutti gli studenti che hanno un totale di voti superiore a 70 e id superiore a 120
+
+const studenti70id120 = students.filter((studente) => {
+  return studente.grades > 70 && studente.id > 120;
+});
+console.table(studenti70id120);
